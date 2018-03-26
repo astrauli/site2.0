ba19723d650302f9c48bff57c163eda063fc2dc7
Aaron Strauli <astrauli@gmail.com>
2018-03-26 16:22:35 -0700

    add video play status listener

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector(".my-name").classList.add("animate-from-left");
+    document.querySelectorAll(".project").forEach(el => {
+        el.addEventListener('click', (e) => {
+            console.log(e.currentTarget)
+            let video = e.currentTarget.getElementsByTagName('video');
+            console.log(video)
+            if (video[0].paused) {
+                video[0].play();
+            } else {
+                video[0].pause();
+            }
+        });
+    })
 });
