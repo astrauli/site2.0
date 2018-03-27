8078888bf6d95a1f66f90432f0ace234cd31cc93
Aaron Strauli <astrauli@gmail.com>
2018-03-26 22:23:02 -0700

    add sliding skill container

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ document.addEventListener('DOMContentLoaded', () => {
             video[0].pause();
         });
     })
+    document.querySelectorAll(".project-skills").forEach(el => {
+        el.addEventListener('click', (e) => {
+             let currentProject = e.currentTarget.nextSibling.nextSibling;
+             let skillContainer = currentProject.querySelector(".skill-set");
+             if(skillContainer.classList.contains("slide-bottom")) {
+                 skillContainer.classList.remove("slide-bottom");
+             } else {
+                 skillContainer.classList.add("slide-bottom")
+             }
+        });
+    });
 });
